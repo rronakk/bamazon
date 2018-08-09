bb65144b026404be3c4e3fa335844155cb0b77e8
ronak <knockronak@gmail.com>
2018-08-09 16:58:55 -0400

    challenge1 implemented

---
+++ bamazon.js
@@ -0,0 +1,118 @@
+require("dotenv").config();
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+var figlet = require('figlet');
+
+
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+
+  port: 3306,
+  user: process.env.DB_USER,
+
+  password: process.env.DB_PASS,
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    showProducts();   
+});
+
+var catalog = new Table({
+    head: ['Product ID', 'Product Name', 'Price', 'Quantity']
+  , colWidths: [15, 30, 10, 10]
+});
+
+function showProducts(){
+    figlet('Amazon - CLI', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            return;
+        }
+        console.log(data);
+        console.log("Following Products are available in our Store : ");
+        connection.query("SELECT * FROM products", function(err, res){
+            if(err) throw err;
+            for (var i in res){
+                catalog.push(
+                    [res[i].id, res[i].product_name, res[i].price, res[i].stock_quantity]
+                );
+            }
+            console.log(catalog.toString());
+            takeOrder(res);
+        });
+    });
+}
+
+
+function takeOrder(res){
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "buy",
+            message: "Would you like to buy anything ?",
+            default: true
+        }
+    ]).then(function(answer){
+        if(answer.buy){
+            inquirer.prompt([
+                {
+                    name: "productID",
+                    message: "Specify Product ID ?",
+                    validate: function validID(name){
+                        if(name > 0 && name <= res.length) return true;
+                            console.log(" \n Please enter valid product ID");
+                            return false;
+                    }
+                },
+                {
+                    name: "quantity",
+                    message: "Enter Quantity",
+                    validate: function valid_entry(name){
+                        if(name>0) return true;
+                        console.log("Please enter quantity greater than 1");
+                        return false;
+                    }
+
+                }
+            ]).then(function(answer){
+                console.log("\n You selected Product ID : " + answer.productID);
+                console.log("\n Item : " + res[answer.productID - 1].product_name)
+                console.log("\n Selected Quantity : " + answer.quantity);
+                if(res[answer.productID - 1].stock_quantity >= answer.quantity){
+                    console.log("\n Total cost of your purchase will be : " + (answer.quantity * res[answer.productID - 1].price));
+                    quantityLeft = res[answer.productID - 1].stock_quantity - answer.quantity;
+                    processOrder(answer.productID, quantityLeft);
+                }
+                else{
+                    console.log("\n Sorry We dont have enough quantity");
+                    connection.end()
+                }
+            })
+        }
+        else{
+            console.log("\n Thanks for visiting");
+            connection.end()
+
+        }
+    })
+}
+
+
+function processOrder(productId, quantity){
+    connection.query("UPDATE products SET ? WHERE ?", [
+        {
+            stock_quantity : quantity,
+        },
+        {
+            id : productId
+        }
+    ], function(err){
+        if(err) throw err;
+        console.log("\n Order Processed !!!!!!");
+    });
+    connection.end();
+}
---
+++ bamazon.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+  id INTEGER(10) NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(20),
+  price DECIMAl(10,4),
+  stock_quantity INTEGER(10),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO PRODUCTS (id, product_name, department_name, price, stock_quantity)
+VALUES (1, "Nike flat wear", "shoes", 29.99, 10);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ( "Apple watch", "watch", 180, 5);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Google pixel 2", "phone", 599, 100);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Levi jeans", "clothes", 59.99, 15);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Samsung Smart 4K TV", "electronics", 500, 6);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Ikea Arm rest chair", "furniture", 59.99, 11);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Coca cola", "drinks", 9.99, 150);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Macbook pro", "computer", 1150, 8);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Everlast notebooks", "stationary", 29.99, 25);
+
+INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity)
+VALUES ("Amazon kindle", "e-reader", 59.99, 40);
+
+SELECT * FROM PRODUCTS;
+
+SELECT stock_quantity FROM `products` WHERE id = 1;
+UPDATE `products` SET stock_quantity = 10 WHERE id = 1;
